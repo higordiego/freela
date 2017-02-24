b6b09dd3f5e9bb7b3d6aaf0e8579c851579fa441
Suissa <jnascimento@gmail.com>
2017-02-23 21:32:24 -0300

    Melhorando o Db

--- _config/mongoDB.js
+++ _config/mongoDB.js
@@ -1,35 +1,47 @@
 const mongoose = require('mongoose')
-
-const online = true;
-const url =  online ? 'mongodb://eyeson:eyeson@ds157439.mlab.com:57439/eyeson' : 'mongodb://localhost/seed-atomic';
-
 mongoose.Promise = require('bluebird')
 
+const MONGODB = {
+  db: `eyeson`
+}
+const online = process.env.ONLINE || false
+// console.log('online', online)
+
+MONGODB.server =  online 
+                    ? `eyeson:eyeson@ds157439.mlab.com:57439` 
+                    : `localhost`
+
+
+const url = `mongodb://${MONGODB.server}/${MONGODB.db}`
+// console.log('url', url)
+// console.log('MONGODB', MONGODB)
+
+
 mongoose.connect(url)
+  .then(() => {
+    console.log('Mongodb Conectado : )')
+  console.log('Dados do servidor: ', MONGODB)
+
+    mongoose.connection.on('error', err =>
+      console.log(`mongoose connection: `+err)
+    )
+
+    mongoose.connection.on('reconnected', () =>
+      console.log('Reconnected to MongoDB')
+    )
+  })
+  .catch(err => {
+    console.log(`rejected promise ${err}`)
+    mongoose.disconnect()      
+  })
+
+process.on('SIGINT', ()=> 
+  mongoose.connection.close( ()=> {
+    console.log('Mongodb: bye : )')
+    process.exit(0)
+  })
+)
 
 
-.then(() =>{
- mongoose.connection.on('error', err =>{
-  console.log(`mongoose connection: `+err)
-})
- mongoose.connection.on('reconnected', ()=> {
-  console.log('Reconnected to MongoDB');
-});
- console.log('Mongodb Conectado : )')
-})
-.catch(err => {
-  console.log(`rejected promise ${err}`)
-  mongoose.disconnect()      
-
-})
-
-process.on('SIGINT', ()=> {
-  mongoose.connection.close(()=> {
-    console.log('Mongodb: bye : )');
-    process.exit(0);
-  });
-});
-
-
-module.exports = mongoose;
+module.exports = mongoose
 
