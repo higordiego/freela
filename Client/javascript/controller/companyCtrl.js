a40021adaeeded8bc9c1a3ba2a26b540f2786570
Higor Diego <higordiegoti@gmail.com>
2017-03-27 18:34:23 -0300

    Subindo create e delete do front company

--- Client/javascript/controller/companyCtrl.js
+++ Client/javascript/controller/companyCtrl.js
@@ -1,9 +1,32 @@
 (function(){
 	'use strict'
-	app.controller('CompanyCtrl', ['$scope','AuthService','$location', 'PainelFactory' ,
-		function($scope,AuthService,$location,PainelFactory){
-			$scope.user = {};
-			$scope.user = AuthService.getUser();
-			
+	app.controller('CompanyCtrl', ['$scope','AuthService','$location', 'PainelFactory' , 'CompanyFactory',
+		function($scope,AuthService,$location,PainelFactory, CompanyFactory){
+			$scope.company = {};
+			$scope.companys = [];
+
+			$scope.add = function(company){
+				CompanyFactory.create(company).then(function(response){
+					$('#myModal').modal('hide');
+					$scope.list()
+					delete $scope.company
+				});
+			}
+
+			$scope.delete = function(company){
+				CompanyFactory.delete(company).then(function(response){
+					delete $scope.companys
+					$scope.list()
+				}, function(err){
+					console.log(err)
+				})
+			}
+
+			$scope.list = function(){
+				CompanyFactory.list().then(function(response){
+					$scope.companys = response.data
+				})
+			}
+
 		}]);
 })();
