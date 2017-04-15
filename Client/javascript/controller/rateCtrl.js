e8a84e78571bb7060d883c5af92d737dfe93f059
Higor Diego <higordiegoti@gmail.com>
2017-04-15 12:40:31 -0300

    myAlert beta

--- Client/javascript/controller/rateCtrl.js
+++ Client/javascript/controller/rateCtrl.js
@@ -3,6 +3,38 @@
 	app.controller('RateCtrl', ['$scope','$location','$state', 'RateFactory'
 		, function($scope,$location,$state,RateFactory){
 			$scope.rate = {};
+			$scope.myAlert = {};
 
+			$scope.myAlerts = [
+			{
+				city: 'Boston',
+				storeNumber: 'Store 60',
+				descriptionStore: 'Excessive Return Transaction',
+				descriptionStoreMessage: 'I am alone, and feel the charm of existence in this spot..',
+				prioritys: [
+				{
+					_id: '232193123921',
+					name: 'Highest'
+				},
+				{
+					_id: '23219312392132131',
+					name: 'Highest priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'High priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Normal priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Low priority'
+				}
+				],
+				priority:'23219312392132132135' 
+			}
+			]
 		}]);
 })();
