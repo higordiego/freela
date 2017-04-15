04b13b616c8912ac6a6d55499d8fc69f137b1f44
Higor Diego <higordiegoti@gmail.com>
2017-04-15 15:18:43 -0300

    subindo

--- Client/javascript/controller/rateCtrl.js
+++ Client/javascript/controller/rateCtrl.js
@@ -34,7 +34,124 @@
 				}
 				],
 				priority:'23219312392132132135' 
+			},
+			{
+				city: 'Boston',
+				storeNumber: 'Store 70',
+				descriptionStore: 'Excessive Return Transaction',
+				descriptionStoreMessage: 'I am alone, and feel the charm of existence in this spot..',
+				prioritys: [
+				{
+					_id: '232193123921',
+					name: 'Highest'
+				},
+				{
+					_id: '23219312392132131',
+					name: 'Highest priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'High priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Normal priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Low priority'
+				}
+				],
+				priority:'23219312392132132135' 
+			},
+			{
+				city: 'Boston',
+				storeNumber: 'Store 70',
+				descriptionStore: 'Excessive Return Transaction',
+				descriptionStoreMessage: 'I am alone, and feel the charm of existence in this spot..',
+				prioritys: [
+				{
+					_id: '232193123921',
+					name: 'Highest'
+				},
+				{
+					_id: '23219312392132131',
+					name: 'Highest priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'High priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Normal priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Low priority'
+				}
+				],
+				priority:'23219312392132132135' 
+			},
+			{
+				city: 'Texas',
+				storeNumber: 'Store 10',
+				descriptionStore: 'Excessive Return Transaction',
+				descriptionStoreMessage: 'I am alone, and feel the charm of existence in this spot..',
+				prioritys: [
+				{
+					_id: '232193123921',
+					name: 'Highest'
+				},
+				{
+					_id: '23219312392132131',
+					name: 'Highest priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'High priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Normal priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Low priority'
+				}
+				],
+				priority:'23219312392132132135' 
+			},
+			{
+				city: 'Texas',
+				storeNumber: 'Store 32',
+				descriptionStore: 'Excessive Return Transaction',
+				descriptionStoreMessage: 'I am alone, and feel the charm of existence in this spot..',
+				prioritys: [
+				{
+					_id: '232193123921',
+					name: 'Highest'
+				},
+				{
+					_id: '23219312392132131',
+					name: 'Highest priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'High priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Normal priority'
+				},
+				{
+					_id: '23219312392132132135',
+					name: 'Low priority'
+				}
+				],
+				priority:'23219312392132132135' 
 			}
 			]
+			$scope.pagination = $scope.myAlerts / 10 == 0 ? '1' : '0'
 		}]);
 })();
